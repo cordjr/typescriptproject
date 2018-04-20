580683e32dbce41ebbbc8b7ff3bb9bbb709705d7
cordjr <jose.cordeiro@armateus.com.br>
2018-04-20 14:53:00 -0300

    implemented view template

--- app/ts/views/View.ts
+++ app/ts/views/View.ts
@@ -1,10 +1,18 @@
-class View {
+class View<T> {
     protected _elemento: Element;
 
 
     constructor(seletor: string) {
         this._elemento = document.querySelector(seletor);
 
+    }
+
+    update(model: T){
+        this._elemento.innerHTML = this.template(model);
+    }
+
+    template(model: T): string{
+        throw new Error(' Voce tem que implementar esse metodo!!!!')
     }
 
 
